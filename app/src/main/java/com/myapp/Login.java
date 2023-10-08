3fb7a4d9612cef976d3934c2cc16597523277e30
PhuongNTK <phuongntk2392@gmail.com>
2023-10-09 06:40:48 +0700

    Xử lý Button Register

--- app/src/main/java/com/myapp/Login.java
+++ app/src/main/java/com/myapp/Login.java
@@ -1,8 +1,18 @@
 package com.myapp;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +22,35 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //Ánh xạ
-        TextInputEditText edtUser = findViewById(R.id.etdUser);
+        TextInputEditText edtUser = findViewById(R.id.edtUser);
+        TextInputLayout txtUser = findViewById(R.id.txtUser);
+        TextInputEditText edtPass = findViewById(R.id.edtPass);
+        TextInputLayout txtPass = findViewById(R.id.txtPass);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegister = findViewById(R.id.btnRegister);
+        CheckBox chkRemember = findViewById(R.id.chkRemember);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, Register.class);
+                myLauncher.launch(intent);
+            }
+        });
     }
+
+    private ActivityResultLauncher<Intent> myLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            //Nơi xử lý dữ liệu trả về regitster
+                            if (result.getResultCode() == 1){
+                                Intent intent = result.getData();
+                                String user = intent.getStringExtra("user");
+                                String pass = intent.getStringExtra("pass");
+                                Toast.makeText(Login.this, user+"-" +pass, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+            });
 }
--- app/src/main/java/com/myapp/Register.java
+++ app/src/main/java/com/myapp/Register.java
@@ -1,7 +1,14 @@
 package com.myapp;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +16,41 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Ánh xạ
+        TextInputEditText edtUser = findViewById(R.id.edtUser);
+        TextInputLayout txtUser = findViewById(R.id.txtUser);
+
+        TextInputEditText edtPass = findViewById(R.id.edtPass);
+        TextInputLayout txtPass = findViewById(R.id.txtPass);
+
+        TextInputEditText edtRePass = findViewById(R.id.edtRePass);
+        TextInputLayout txtRePass = findViewById(R.id.txtRePass);
+
+        Button btnResgiter = findViewById(R.id.btnResgiter);
+        Button btnBack = findViewById(R.id.btnBack);
+
+        //Bắt sự kiện cho Resgister
+        btnResgiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = edtUser.getText().toString();
+                String pass = edtPass.getText().toString();
+                String rePass = edtRePass.getText().toString();
+                //Validate
+                if (user.equals("") || pass.equals("") || rePass.equals("")) {
+                    Toast.makeText(Register.this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                } else if (!pass.equals(rePass)) {
+                    Toast.makeText(Register.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent();
+                    intent.putExtra("user", user);
+                    intent.putExtra("pass", pass);
+                    setResult(1, intent);
+                    finish();
+                }
+            }
+        });
+
+        }
     }
-}
