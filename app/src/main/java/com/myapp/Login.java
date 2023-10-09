8e4a74e83fcfd955545e5a2f1a270c0a0bc5bae4
PhuongNTK <phuongntk2392@gmail.com>
2023-10-09 19:38:59 +0700

    Validate Login and Register

--- app/src/main/java/com/myapp/Login.java
+++ app/src/main/java/com/myapp/Login.java
@@ -1,6 +1,9 @@
 package com.myapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -16,6 +19,8 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class Login extends AppCompatActivity {
 
+    String userRegister ="";
+    String passRegister = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,47 @@ public class Login extends AppCompatActivity {
         Button btnLogin = findViewById(R.id.btnLogin);
         Button btnRegister = findViewById(R.id.btnRegister);
         CheckBox chkRemember = findViewById(R.id.chkRemember);
+
+        //Kiểm tra thông tin đăng nhập người dùng có lưu lại không??
+        SharedPreferences sharedPreferences = getSharedPreferences("INFO",MODE_PRIVATE);
+        boolean isRemember = sharedPreferences.getBoolean("isRemember",false);
+        if(isRemember){
+            String user = sharedPreferences.getString("userLogin","");
+            String pass = sharedPreferences.getString("passLogin","");
+            edtUser.setText(user);
+            edtPass.setText(pass);
+            chkRemember.setChecked(isRemember);
+            userRegister = user;
+            passRegister = pass;
+        }
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userLogin = edtUser.getText().toString();
+                String passLogin = edtPass.getText().toString();
+                if(userLogin.length() > 0 && passLogin.length() > 0 && userLogin.equals(userRegister) && passLogin.equals(passRegister)){
+                    //check remember me
+                    if(chkRemember.isChecked()){
+                        SharedPreferences preferences = getSharedPreferences("INFO",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("userLogin", userLogin);
+                        editor.putString("passLogin", passLogin);
+                        editor.putBoolean("isRemember",chkRemember.isChecked());
+                        editor.apply();
+                    }else {
+                        SharedPreferences preferences = getSharedPreferences("INFO",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.clear();
+                        editor.apply();
+                    }
+                    Toast.makeText(Login.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                }else {
+                    Toast.makeText(Login.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,12 +91,12 @@ public class Login extends AppCompatActivity {
                         @Override
                         public void onActivityResult(ActivityResult result) {
                             //Nơi xử lý dữ liệu trả về regitster
-                            if (result.getResultCode() == 1){
+                            if (result.getResultCode() == 1) {
                                 Intent intent = result.getData();
-                                String user = intent.getStringExtra("user");
-                                String pass = intent.getStringExtra("pass");
-                                Toast.makeText(Login.this, user+"-" +pass, Toast.LENGTH_SHORT).show();
+                                userRegister = intent.getStringExtra("user");
+                                passRegister = intent.getStringExtra("pass");
                             }
                         }
-            });
+                    });
+
 }
--- app/src/main/java/com/myapp/Register.java
+++ app/src/main/java/com/myapp/Register.java
@@ -1,6 +1,8 @@
 package com.myapp;
 
 import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -30,6 +32,70 @@ public class Register extends AppCompatActivity {
         Button btnResgiter = findViewById(R.id.btnResgiter);
         Button btnBack = findViewById(R.id.btnBack);
 
+        //Bắt Validate edtUser
+        edtUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(s.length() == 0){
+                    txtUser.setError("Vui lòng nhập Username");
+                }else {
+                    txtUser.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        //Bắt Validate edtPass
+        edtPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(s.length() == 0){
+                    txtPass.setError("Vui lòng nhập Password");
+                }else {
+                    txtPass.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //Bắt Validate edtRePass
+        edtRePass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int i, int i1, int i2) {
+                if(s.length() == 0){
+                    txtRePass.setError("Vui lòng nhập lại Password");
+                }else {
+                    txtRePass.setError(null);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         //Bắt sự kiện cho Resgister
         btnResgiter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +105,20 @@ public class Register extends AppCompatActivity {
                 String rePass = edtRePass.getText().toString();
                 //Validate
                 if (user.equals("") || pass.equals("") || rePass.equals("")) {
-                    Toast.makeText(Register.this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(Register.this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                    if (user.equals("")) {
+                        txtUser.setError("Vui lòng nhập Username");
+                    }else {
+                        txtUser.setError(null);
+                    }if (pass.equals("")){
+                        txtPass.setError("Vui lòng nhập Password");
+                    }else {
+                        txtPass.setError(null);
+                    }if( rePass.equals("")){
+                        txtRePass.setError("Vui lòng nhập lại Password");
+                    }else {
+                        txtRePass.setError(null);
+                    }
                 } else if (!pass.equals(rePass)) {
                     Toast.makeText(Register.this, "Mật khẩu không trùng khớp", Toast.LENGTH_SHORT).show();
                 } else {
@@ -51,6 +130,5 @@ public class Register extends AppCompatActivity {
                 }
             }
         });
-
-        }
-    }
+     }
+}
